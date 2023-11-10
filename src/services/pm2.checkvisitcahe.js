330b7eda940314b1440a5caea31185b80b278767
tumzaaa2009 <tumzaaa_@hotmail.com>
2023-11-10 11:11:37 +0700

    crontab

--- src/services/pm2.checkvisitcahe.js
+++ src/services/pm2.checkvisitcahe.js
@@ -3,38 +3,43 @@ const moment = require('moment');
 const https = require('https')
 const { config } = require('dotenv');
 config({ path: '.env.production.local' });
-
+const cron = require('node-cron');
 const { END_POINT, URL_Hos, Token_DrugAllgy } = process.env;
 
 const checkAndRun = async () => {
   try {
     const response = await axios.get(`${END_POINT}/checkvisitcashe/`, {
       headers: {
-        'x-api-key': Token_DrugAllgy
+        'x-api-key': `${Token_DrugAllgy}`
       }
+
     });
-    
     const dateEvent = response.data.dateEvent;
-    console.log(dateEvent)
     if (moment().format("YYYY-MM-DD HH:mm:ss") >= dateEvent) {
-      console.log("tsss")
-      try {
-        await axios.post(`${URL_Hos}/hie/visitcashe` );
-        console.log('HTTP request to ' + URL_Hos + '/hie/visitcashe has been made.');
-        // เพิ่มโค้ดเพิ่มเติมที่คุณต้องการจากการเรียก HTTP นี้
-      } catch (error) {
-        console.error('HTTP request to ' + URL_Hos + '/hie/visitcashe failed:', error);
-      }
+      // หากเราอยู่หรือเกินวันและเวลาที่กำหนด
+      axios.post(`${URL_Hos}/hie/visitcashe`, null, {
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+      })
+        .then((response) => {
+          console.log('HTTP request to http://localhost:3000/hie/drugallgycashe has been made.');
+          // ใส่โค้ดเพิ่มเติมที่คุณต้องการทำหลังจากการเรียก HTTP นี้
+        })
+        .catch((error) => {
+          console.error('HTTP request to http://localhost:3000/hie/drugallgycashe failed:', error);
+        });
     } else {
       console.log('Not yet time to make the HTTP request.');
     }
+
   } catch (error) {
-    console.error('An error occurred:', error);
+
   }
-    
-  
+
 
 };
 
 // เรียกฟังก์ชัน checkAndRun เพื่อเริ่มต้นตรวจสอบและการรัน
-checkAndRun();
+cron.schedule('11 11 * * *', () => {
+  console.log('Running checkAndRun at 11:30 AM every day');
+  checkAndRun();
+});
